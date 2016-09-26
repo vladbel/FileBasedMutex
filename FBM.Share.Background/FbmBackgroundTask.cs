55806ec96c02a7af92e860ea2739b1a0e5fda405
Vladimir Beliaev <vladbel@live.com>
2016-09-26 17:46:33 -0500

    FBM: initial named mutex implementation

--- FBM.Share.Background/FbmBackgroundTask.cs
+++ FBM.Share.Background/FbmBackgroundTask.cs
@@ -5,6 +5,8 @@ using Windows.Foundation;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System;
 using System.Diagnostics;
+using FBM.Kit.Services;
+using FBM.Services;
 
 namespace FBM.Background
 {
@@ -12,7 +14,26 @@ namespace FBM.Background
     {
         void IBackgroundTask.Run(IBackgroundTaskInstance taskInstance)
         {
-            Debug.WriteLine("FbmBackgroundTask.Run()");
+            Debug.WriteLine("FbmBackgroundTask.Run(): Start");
+
+            IMutex mutex = new RefreshTokenMutex();
+
+            try
+            {
+                Debug.WriteLine("FbmBackgroundTask.Run(): enter mutex");
+                mutex.Enter();
+                //Do work
+                Debug.WriteLine("FbmBackgroundTask.Run(): do work");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("FbmBackgroundTask.Run(): exception {0}", ex.ToString());
+            }
+            finally
+            {
+                Debug.WriteLine("FbmBackgroundTask.Run(): release mutex");
+                mutex.Release();
+            }
         }
     }
 }
---
+++ FBM.Shared.Services/RefreshTokenMutex.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using FBM.Kit.Services;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FBM.Services
+{
+    internal class RefreshTokenMutex : IMutex
+    {
+        public const string REFRESH_TOKEN_MUTEX_NAME = "REFRESH_TOKEN_MUTEX_NAME";
+        private string _name;
+        private Mutex _mutex = null;
+
+        public RefreshTokenMutex ()
+        {
+            _name = REFRESH_TOKEN_MUTEX_NAME;
+        }
+        public void Enter()
+        {
+            try
+            {
+                _mutex = Mutex.OpenExisting(_name);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+
+            if (_mutex == null)
+            {
+                CreateMutex();
+            }
+            else
+            {
+                _mutex.WaitOne();
+            }
+        }
+
+        public void Release()
+        {
+            if (_mutex != null)
+            {
+                _mutex.ReleaseMutex();
+                _mutex.Dispose();
+                _mutex = null;
+            }
+
+        }
+
+        private void CreateMutex()
+        {
+            bool mutexCreated = false;
+            _mutex = new Mutex(true , _name, out mutexCreated);
+
+            if (!mutexCreated)
+            {
+                throw new Exception("Can't create named  mutex");
+            }
+        }
+    }
+}
