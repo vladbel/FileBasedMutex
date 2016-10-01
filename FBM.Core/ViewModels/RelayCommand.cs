675107b518c01f5dbf39f116d5582c40d006cfbb
Vladimir Beliaev <vladbel@live.com>
2016-10-01 15:07:26 -0500

    FBM: Base VM and relay command

---
+++ FBM.Core/ViewModels/RelayCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FBM.Core.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _handler;
+        private bool _isEnabled;
+
+        public RelayCommand(Action handler)
+        {
+            _handler = handler;
+            IsEnabled = true;
+        }
+
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                if (value != _isEnabled)
+                {
+                    _isEnabled = value;
+                    if (CanExecuteChanged != null)
+                    {
+                        CanExecuteChanged(this, EventArgs.Empty);
+                    }
+                }
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return IsEnabled;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _handler();
+        }
+    }
+
+}
---
+++ FBM.Core/ViewModels/ViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBM.Core.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
